a9dfbbe28282af66e727bfbaf85bbf88e0967d7d
thearjunl <arjunl2026@mca.ajce.in>
2026-03-11 12:40:41 +0530

    fix: add GET to CORS allow_methods for /model/info endpoint

--- ml/api_server.py
+++ ml/api_server.py
@@ -79,7 +79,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
-    allow_methods=["POST"],
+    allow_methods=["GET", "POST"],
     allow_headers=["Content-Type"],
 )
 
